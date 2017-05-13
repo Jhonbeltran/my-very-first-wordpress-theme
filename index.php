328a8869fe493a1692360e559df095ac8ded4ba3
Jhonbeltran <jbeltranleon@gmail.com>
2017-05-13 14:05:06 -0500

    Agrego implementacion de la funcion bloginfo()

--- index.php
+++ index.php
@@ -2,9 +2,25 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title><?php bloginfo(title); ?></title>
 </head>
 <body>
-	<h1>Vamos a iniciar a crear el tema :D</h1>
+	<center>
+		<h3>
+			<?php bloginfo(description); ?>
+		</h3>
+	</center>
+	<center>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo ea sed architecto vitae ab aliquid dolores autem, accusantium labore repudiandae natus. Ratione sapiente molestias, itaque accusamus tempora, voluptatem! Quasi, quaerat.</p>
+	</center>
+	
+	<center>
+		<ul>
+			<li>Url: <?php bloginfo(url); ?></li>
+			<li>Idioma <?php bloginfo(language); ?></li>
+			<li>Archivo de css: <?php bloginfo(stylesheet_url); ?></li>
+			<li>Directorio de archivos css: <?php bloginfo(stylesheet_directory); ?></li>
+		</ul>
+	</center>
 </body>
 </html>
