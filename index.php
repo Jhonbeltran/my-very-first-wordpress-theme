64ea2e3538bb8325dc439d4ed697317ad7ed8b38
Jhonbeltran <jbeltranleon@gmail.com>
2017-05-20 14:39:35 -0500

    Uso filtros en el The_Loop para mostrar solo entradas de cierta categoria

--- index.php
+++ index.php
@@ -9,24 +9,35 @@
             Mis Proyectos más recientes
           </h4>
           <div class="divider"></div>
+            <?php
+              $args = array('cat' => 4);
+              $category_posts = new WP_Query($args);
 
-          <?php if (have_posts()){
-            while (have_posts()) {
-              the_post(); ?>
-              <article>
-                <h5 style="text-transform: uppercase !important;">
-                  <?php the_title(); ?>
-                </h5>
-                <div class="flow-text blue-grey-text"><?php the_excerpt(); ?></div>
-                <footer>
-                  <blockquote>
-                    <?php the_tags(); ?> @<?php the_author(); ?>
-                  </blockquote>
-                </footer>
-              </article>
-              <?php
-            }
-          }?>
+              if($category_posts->have_posts()):
+                while($category_posts->have_posts()):
+                  $category_posts->the_post();
+            ?>
+                  <article>
+                    <h5 style="text-transform: uppercase !important;">
+                      <?php the_title(); ?>
+                    </h5>
+                    <div class="flow-text blue-grey-text"><?php the_excerpt(); ?></div>
+                    <footer>
+                      <blockquote>
+                        <?php the_tags(); ?> @<?php the_author(); ?>
+                      </blockquote>
+                    </footer>
+                  </article>
+            <?php 
+                endwhile;
+              else:
+            ?>
+                <p class="flow-text">
+                  No encontramos entradas 😱
+                </p>
+            <?php 
+              endif;
+            ?>
 
         </div>
       </div>
